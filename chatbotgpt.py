2b6f15cc38c08439da6ebd2b3a6d83f3348a1c18
Tran Nguyen <haihuongtran.ng@gmail.com>
2025-01-24 15:44:32 +0100

    selectbox

--- chatbotgpt.py
+++ chatbotgpt.py
@@ -40,3 +40,13 @@ if prompt := st.chat_input("What is up?"):
         )
         response = st.write_stream(stream)
     st.session_state.messages.append({"role": "assistant", "content": response})
+
+# Select box
+import streamlit as st
+
+option = st.selectbox(
+    "Quel type de modèle de ChatGPT?",
+    ("gpt-3.5-turbo", "gpt-3.5-turbo-instruct", "gpt-3.5-turbo-1106", "gpt-3.5-turbo-0125"),
+    index=None,
+    placeholder="Choisir un modèle...",
+)
